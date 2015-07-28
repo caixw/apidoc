4f2452ac55a8ce1f7a07794867c422f0c0dba57d
caixw <xnotepad@gmail.com>
2015-07-28 21:59:38 +0800

    不再直接输出json内容。 chrome不能加载本地的json数据。所以不再使用输出json数据，然后通过ajax加载相应的json数据的形式，而是直接使用go的模板输出内容。

--- core/types.go
+++ core/types.go
@@ -9,53 +9,53 @@ import (
 )
 
 type Tree struct {
-	Docs map[string][]*doc `json:"docs"` // 按apiGroup分组的文档结构。
-	Date time.Time         `json:"date"` // 编译时间。
+	Docs map[string][]*doc // 按apiGroup分组的文档结构。
+	Date time.Time         // 编译时间。
 }
 
 // 表示一个api文档。
 type doc struct {
-	Group       string    `json:"group"`       // 所属分组
-	Version     string    `json:"version"`     // 版本号
-	Methods     string    `json:"methods"`     // 请求的方法，GET，POST等
-	URL         string    `json:"url"`         // 请求地址
-	Summary     string    `json:"summary"`     // 简要描述
-	Description string    `json:"description"` // 详细描述
-	Queries     []*param  `json:"queries"`     // 查询参数
-	Params      []*param  `json:"params"`      // URL参数
-	Request     *request  `json:"request"`     // 若是GET，则使用此描述请求的具体数据
-	Status      []*status `json:"status"`      // 各种状态码下返回的数据描述
+	Group       string    // 所属分组
+	Version     string    // 版本号
+	Methods     string    // 请求的方法，GET，POST等
+	URL         string    // 请求地址
+	Summary     string    // 简要描述
+	Description string    // 详细描述
+	Queries     []*param  // 查询参数
+	Params      []*param  // URL参数
+	Request     *request  // 若是GET，则使用此描述请求的具体数据
+	Status      []*status // 各种状态码下返回的数据描述
 }
 
 type request struct {
-	Type     string            `json:"type"`    // 请求的类型，xml或是json
-	Headers  map[string]string `json:"headers"` // 请求必须携带的头
-	Params   []*param          `json:"params"`  //提交的各个字段的描述
-	Examples []*example        `json:"examples"`
+	Type     string            // 请求的类型，xml或是json
+	Headers  map[string]string // 请求必须携带的头
+	Params   []*param          //提交的各个字段的描述
+	Examples []*example
 }
 
 // 表示一次请求或是返回的数据。
 type status struct {
-	Code     string            `json:"code"`     // 状态码
-	Type     string            `json:"type"`     // 提交或是返回的数据类型,xml或是json
-	Summary  string            `json:"summary"`  // 该状态下的简要描述
-	Headers  map[string]string `json:"headers"`  // 必须提交的头信息或是返回的头信息。
-	Params   []*param          `json:"params"`   // 提交或是返回数据的各个字段描述
-	Examples []*example        `json:"examples"` // 提交或是返回数据的示例，键名为数据类型，键值为示例代码
+	Code     string            // 状态码
+	Type     string            // 提交或是返回的数据类型,xml或是json
+	Summary  string            // 该状态下的简要描述
+	Headers  map[string]string // 必须提交的头信息或是返回的头信息。
+	Params   []*param          // 提交或是返回数据的各个字段描述
+	Examples []*example        // 提交或是返回数据的示例，键名为数据类型，键值为示例代码
 }
 
 // 用于描述提交和返回的参数信息。
 type param struct {
-	Name        string `json:"name"`        // 参数名称
-	Type        string `json:"type"`        // 类型
-	Optional    bool   `json:"optional"`    // 可选的参数
-	Description string `json:"description"` // 参数介绍
+	Name        string // 参数名称
+	Type        string // 类型
+	Optional    bool   // 可选的参数
+	Description string // 参数介绍
 }
 
 // 示例代码
 type example struct {
-	Type string `json:"type"` // 示例代码的类型，小写，xml或是json
-	Code string `json:"code"` // 示例代码
+	Type string // 示例代码的类型，小写，xml或是json
+	Code string // 示例代码
 }
 
 func NewTree() *Tree {
