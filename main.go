3d41edc83de1f7f14785dc71a6c8477a96f99512
caixw <xnotepad@gmail.com>
2015-08-22 23:21:39 +0800

    将print.go的代码并入到main.go中

--- main.go
+++ main.go
@@ -2,43 +2,44 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-// apidoc从代码注释中提取并生成api的文档。
+// apidoc是一个RESTful api文档生成工具。
 package main
 
 import (
 	"flag"
+	"runtime"
 	"time"
 
 	"github.com/caixw/apidoc/core"
 	o "github.com/caixw/apidoc/output"
+	"github.com/issue9/term/colors"
 )
 
-const version = "0.7.33.150822"
+const version = "0.7.34.150822"
+
+const (
+	out          = colors.Stdout
+	titleColor   = colors.Green
+	contentColor = colors.Default
+	errorColor   = colors.Red
+	warnColor    = colors.Cyan
+)
+
+const usage = `apidoc是一个RESTful api文档生成工具。
+
+命令行语法:
+ apidoc [options]
+
+options:
+ -h       显示当前帮助信息；
+ -v       显示apidoc和go程序的版本信息；
+ -l       显示所有支持的语言类型。
+ -r       是否搜索子目录，默认为true；
+
+有关apidoc的详细信息，可访问官网：https://caixw.github.io/apidoc`
 
 func main() {
-	var (
-		h     bool
-		v     bool
-		langs bool
-		r     bool
-	)
-
-	flag.Usage = printUsage
-	flag.BoolVar(&h, "h", false, "显示帮助信息")
-	flag.BoolVar(&v, "v", false, "显示帮助信息")
-	flag.BoolVar(&langs, "langs", false, "显示所有支持的语言")
-	flag.BoolVar(&r, "r", true, "搜索子目录，默认为true")
-	flag.Parse()
-
-	switch {
-	case h:
-		flag.Usage()
-		return
-	case v:
-		printVersion()
-		return
-	case langs:
-		printLangs()
+	if flags() {
 		return
 	}
 
@@ -75,4 +76,59 @@ func main() {
 		printError(err)
 		return
 	}
+}
+
+// 处理命令行参数，若被处理，返回true，否则返回false。
+func flags() (ok bool) {
+	var h, v, l bool
+
+	flag.Usage = printUsage
+	flag.BoolVar(&h, "h", false, "显示帮助信息")
+	flag.BoolVar(&v, "v", false, "显示帮助信息")
+	flag.BoolVar(&l, "l", false, "显示所有支持的语言")
+	flag.Parse()
+
+	switch {
+	case h:
+		flag.Usage()
+		return true
+	case v:
+		printVersion()
+		return true
+	case l:
+		printLangs()
+		return true
+	}
+	return false
+}
+
+func printUsage() {
+	colors.Println(out, contentColor, colors.Default, usage)
+}
+
+func printError(msg ...interface{}) {
+	colors.Println(out, errorColor, colors.Default, msg...)
+}
+
+func printSyntaxErrors(errs []error) {
+	for _, v := range errs {
+		colors.Println(out, warnColor, colors.Default, v)
+	}
+}
+
+func printLangs() {
+	colors.Println(out, titleColor, colors.Default, "目前支持以下类型的代码解析:")
+	for k, v := range langs {
+		colors.Print(out, titleColor, colors.Default, k, ":")
+		colors.Println(out, contentColor, colors.Default, v.exts)
+	}
+}
+
+func printVersion() {
+	colors.Print(out, titleColor, colors.Default, "apidoc: ")
+	colors.Println(out, contentColor, colors.Default, version)
+
+	colors.Print(out, titleColor, colors.Default, "Go: ")
+	goVersion := runtime.Version() + " " + runtime.GOOS + "/" + runtime.GOARCH
+	colors.Println(out, contentColor, colors.Default, goVersion)
 }
