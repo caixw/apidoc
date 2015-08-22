9176997628bf303468999499722ba3d80ecb633f
caixw <xnotepad@gmail.com>
2015-08-22 20:35:03 +0800

    将打印usage的函数移动到print.go文件

--- print.go
+++ print.go
@@ -18,6 +18,33 @@ const (
 	warnColor    = colors.Cyan
 )
 
+const usage = `apidoc从代码注释中提取并生成api的文档。
+
+命令行语法:
+ apidoc [options] src doc
+
+options:
+ -h       显示当前帮助信息；
+ -v       显示apidoc和go程序的版本信息；
+ -langs   显示所有支持的语言类型。
+ -r       是否搜索子目录，默认为true；
+ -t       目标文件类型，支持的类型可以通过-langs来查看；
+ -version 指定文档的版本号；
+ -title   指定文档的标题；
+ -ext     需要分析的文件的扩展名，若不指定，则会根据-t参数自动生成相应的扩展名。
+          若-t也未指定，则会根据src目录下的文件，自动判断-t的值。
+
+src:
+ 源文件所在的目录。
+doc:
+ 产生的文档保存的目录。
+
+源代码采用MIT开源许可证，并发布于github:https://github.com/caixw/apidoc`
+
+func printUsage() {
+	colors.Println(out, contentColor, colors.Default, usage)
+}
+
 func printError(msg ...interface{}) {
 	colors.Println(out, errorColor, colors.Default, msg...)
 }
