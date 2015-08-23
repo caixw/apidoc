dca997a1f653976fccfb38227a2bf6fc1144fff4
caixw <xnotepad@gmail.com>
2015-08-23 21:13:44 +0800

    美化pre标签的内容

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 "use strict";
 
 $(document).ready(function(){
-    // 按4的倍数去掉pre中多余的空格
     $('pre').each(function(index, elem){
-        var text = $(elem).html().replace(/    /g,'');
+        // 去多余的前导空格，由data-indent属性指定缩进多少个tab
+        var indent = $(this).attr('data-indent');
+        var pattern = '';
+        for(var i=0; i<indent; i++){
+            pattern += '    ';
+        }
+        var text = $(elem).html().replace(new RegExp(pattern, 'g'), '');
+
+        text = text.replace(/ /g, '&#160;');
+        text = text.replace(/(@\w+)/g, '<span class="keyword">$1</span>');
         $(elem).html(text);
     });
 });
