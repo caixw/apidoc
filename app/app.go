69baebf53003588eec6e0015aa9365daf39910d7
caixw <xnotepad@gmail.com>
2016-06-04 02:09:13 +0800

    尝试将部分公用内容和公共配置放到app包下

---
+++ app/app.go
@@ -0,0 +1,19 @@
+// Copyright 2016 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package app
+
+const (
+	// 程序的正式名称
+	AppName = "apidoc"
+
+	// 版本号
+	//
+	// 版本号按照 http://semver.org/lang/zh-CN/ 中的规则，分成以下四个部分：
+	// 主版本号.次版本号.修订号.修订日期
+	Version = "2.2.57.160604"
+
+	// 配置文件名称。
+	ConfigFilename = ".apidoc.json"
+)
---
+++ app/errors.go
@@ -0,0 +1,18 @@
+// Copyright 2016 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package app
+
+import "fmt"
+
+// SyntaxError 语法错误
+type SyntaxError struct {
+	File    string // 发生错误的文件名
+	Line    int    // 发生错误的行号
+	Message string // 具体错误信息
+}
+
+func (err *SyntaxError) Error() string {
+	return fmt.Sprintf("在[%v:%v]出现语法错误[%v]", err.File, err.Line, err.Message)
+}
