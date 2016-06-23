00ff535aa9fb08650a33725948291f69c6a0739f
caixw <xnotepad@gmail.com>
2016-06-23 23:21:57 +0800

    更新文档

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 $(document).ready(function(){
     /* 高亮和格式化代码块 */
@@ -10,9 +10,20 @@ $(document).ready(function(){
             pattern += '    ';
         }
         var text = $(elem).html().replace(new RegExp(pattern, 'g'), '');
-
+        // 替换空格必须在高亮之前，高亮代码会添加 html 属性，用到空格
         text = text.replace(/ /g, '&#160;');
-        text = text.replace(/(@\w+)/g, '<span class="keyword">$1</span>');
+
+        // 高亮代码
+        var lang = $(elem).attr('data-language');
+        if (lang == 'json'){
+            text = text.replace(/(\"[^"']*\")(:)(.+)(,?)/g, '<span class="keyword">$1</span>$2$3$4').
+                replace(/(:[&#160;]*)\"([^"']*)\"(,?)/g, '$1<span class="string">"$2"</span>$3').
+                replace(/(:[&#160;]*)(true|false)(,?)/g, '$1<span class="bool">$2</span>$3')
+
+        }else{ // 未指定
+            text = text.replace(/(@\w+)/g, '<span class="keyword">$1</span>');
+        }
+
         $(elem).html(text);
     });
 });
