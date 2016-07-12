e0ac6a7b76e991cfd2c69e484ee788e1d9e12958
caixw <xnotepad@gmail.com>
2016-07-12 22:43:52 +0800

    去掉一些相同类型的语言测试内容

--- input/input_test.go
+++ input/input_test.go
@@ -15,8 +15,6 @@ import (
 func TestParse(t *testing.T) {
 	a := assert.New(t)
 
-	testParse(a, "c++")
-	testParse(a, "d")
 	testParse(a, "go")
 	testParse(a, "java")
 	testParse(a, "javascript")
@@ -62,8 +60,6 @@ func testParse(a *assert.Assertion, lang string) {
 func TestParseFile(t *testing.T) {
 	a := assert.New(t)
 
-	testParseFile(a, "c++", "./testdata/c++/test1.c")
-	testParseFile(a, "d", "./testdata/d/test1.d")
 	testParseFile(a, "go", "./testdata/go/test1.go")
 	testParseFile(a, "java", "./testdata/java/test1.java")
 	testParseFile(a, "javascript", "./testdata/javascript/test1.js")
