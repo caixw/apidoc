a956bfe0fa88846d6017a4fc60fbbb8999d04d9c
caixw <xnotepad@gmail.com>
2016-07-24 18:05:08 +0800

    修正编译错误

--- locale/locale.go
+++ locale/locale.go
@@ -10,10 +10,6 @@ package locale
 
 import (
 	"errors"
-	"os"
-	"runtime"
-	"syscall"
-	"unsafe"
 
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
@@ -43,34 +39,4 @@ func Init(defaultLang string) error {
 	tag := language.MustParse(localeName)
 	localePrinter = message.NewPrinter(tag)
 	return nil
-}
-
-func getLocaleName() (string, error) {
-	if runtime.GOOS == "windows" {
-		return getWindowsLocale()
-	}
-
-	return os.Getenv("LC_ALL"), nil
-}
-
-func getWindowsLocale() (string, error) {
-	k32, err := syscall.LoadDLL("kernel32.dll")
-	if err != nil {
-		return "", err
-	}
-	defer k32.Release()
-
-	f, err := k32.FindProc("GetUserDefaultLocaleName")
-	if err != nil {
-		return "", err
-	}
-
-	l := 85
-	buf := make([]uint16, l)
-	r1, _, err := f.Call(uintptr(unsafe.Pointer(&buf[0])), uintptr(l))
-	if uint32(r1) == 0 {
-		return "", err
-	}
-
-	return syscall.UTF16ToString(buf), nil
 }
---
+++ locale/locale_posix.go
@@ -0,0 +1,13 @@
+// Copyright 2016 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// +build !windows
+
+package locale
+
+import "os"
+
+func getLocaleName() (string, error) {
+	return os.Getenv("LC_ALL"), nil
+}
