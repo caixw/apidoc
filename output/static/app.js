2818a294fe3fefcce821ea02934106931462708a
caixw <xnotepad@gmail.com>
2017-11-26 09:34:22 +0800

    去掉不需要的代码

--- output/static/app.js
+++ output/static/app.js
@@ -17,22 +17,6 @@ $(document).ready(function(){
         $(elem).html(text);
     });
 
-    // 隐藏当前页面用不到的过滤器
-    $('header .filter input').each(function(index, elem){
-        var val = $(elem).val();
-        if ($('.main .api .method.'+val).length == 0) {
-            $(elem).parent().hide();
-        }
-    });
-
-    // 按请求方法过滤
-    $('header .filter input').on('change', function(){
-        var val = $(this).attr('value');
-        $('.main .api .method.'+val).parents('.api').each(function(index, elem){
-            $(elem).slideToggle();
-        });
-    })
-
     // 按分组跳转页面
     $('#groups').on('change', function(){
         window.location.href = $(this).find('option:selected').val();
