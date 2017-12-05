9ef695f914e367cfdb140fa3c0af9f627b40a78b
caixw <xnotepad@gmail.com>
2017-12-05 23:13:50 +0800

    locale 内部使用 NewPrinter 代替 message.NewPrinter

--- locale/locale.go
+++ locale/locale.go
@@ -15,11 +15,14 @@ import (
 	"golang.org/x/text/message"
 )
 
+// 保证有个初始化的值，部分包的测试功能依赖此变量
+var localePrinter *message.Printer = message.NewPrinter(language.MustParse(vars.DefaultLocale))
+
 // Init 初始化 locale 包并。
 // 无论是否返回错误信息，都会初始一种语言作为其交互语言。
 func Init() error {
 	tag, err := getTag()
-	localePrinter = message.NewPrinter(tag)
+	localePrinter = NewPrinter(tag)
 
 	return err
 }
