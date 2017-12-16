510346ebd90d7626e3370d352c51963275362b55
caixw <xnotepad@gmail.com>
2017-12-16 15:49:13 +0800

    合并文件

--- types/types.go
+++ types/types.go
@@ -5,7 +5,22 @@
 // Package types 一些公用类型的定义
 package types
 
+import (
+	"github.com/caixw/apidoc/locale"
+	"github.com/caixw/apidoc/vars"
+)
+
 // Sanitizer 配置项的检测接口
 type Sanitizer interface {
 	Sanitize() *OptionsError
+}
+
+// OptionsError 提供对配置项错误的描述
+type OptionsError struct {
+	Field   string
+	Message string
+}
+
+func (err *OptionsError) Error() string {
+	return locale.Sprintf(locale.OptionsError, vars.ConfigFilename, err.Field, err.Message)
 }
