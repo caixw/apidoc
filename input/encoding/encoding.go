c99fc1c461b77a6abe07934716f8455bd9b284f3
caixw <xnotepad@gmail.com>
2017-12-23 13:21:52 +0800

    添加对输入文件编码的处理

---
+++ input/encoding/encoding.go
@@ -0,0 +1,67 @@
+// Copyright 2017 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package encoding
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"strings"
+
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/traditionalchinese"
+	"golang.org/x/text/transform"
+)
+
+// 一个编码名称与解码器的关联。
+// 若解码器为空，表示不需要解码。
+var encodings = map[string]encoding.Encoding{
+	"utf8":  nil,
+	"utf-8": nil,
+
+	"gbk":     simplifiedchinese.GBK,
+	"gb18083": simplifiedchinese.GB18030,
+	"gb2312":  simplifiedchinese.HZGB2312,
+
+	"big5": traditionalchinese.Big5,
+}
+
+// Encodings 返回支持的编码列表
+func Encodings() []string {
+	ret := make([]string, 0, len(encodings))
+
+	for name := range encodings {
+		ret = append(ret, name)
+	}
+
+	return ret
+}
+
+// Transform 将 path 指向的文件内容，按 encoding 编码进行加载，
+// 并转换成 utf-8 之后返回其内容。
+func Transform(path, encoding string) ([]byte, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	enc, found := encodings[strings.ToLower(encoding)]
+	if !found {
+		return nil, errors.New("not found")
+	}
+
+	if enc == nil {
+		return data, nil
+	}
+
+	reader := transform.NewReader(bytes.NewReader(data), enc.NewDecoder())
+	ret, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return ret, nil
+}
