49d3bb7f5c75f53c50b8f68d699f8bee4aa10d4a
caixw <xnotepad@gmail.com>
2018-10-27 20:55:34 +0800

    完成转换成 openapi 的功能
    close #43

--- output/openapi/parse.go
+++ output/openapi/parse.go
@@ -6,6 +6,7 @@ package openapi
 
 import (
 	"encoding/json"
+	"strings"
 
 	yaml "gopkg.in/yaml.v2"
 
@@ -24,8 +25,6 @@ func parse(doc *doc.Doc) (*OpenAPI, error) {
 		},
 		Servers: make([]*Server, 0, len(doc.Servers)),
 		Tags:    make([]*Tag, 0, len(doc.Tags)),
-
-		// TODO Paths
 	}
 
 	for _, srv := range doc.Servers {
@@ -36,7 +35,173 @@ func parse(doc *doc.Doc) (*OpenAPI, error) {
 		openapi.Tags = append(openapi.Tags, newTag(tag))
 	}
 
+	if err := parsePaths(openapi, doc); err != nil {
+		return nil, err
+	}
+
 	return openapi, nil
+}
+
+func parsePaths(openapi *OpenAPI, doc *doc.Doc) *Error {
+	for _, api := range doc.Apis {
+		p := openapi.Paths[api.Path]
+		if p == nil {
+			p = &PathItem{}
+			openapi.Paths[api.Path] = p
+		}
+
+		operation, err := setOperation(p, api.Method)
+		if err != nil {
+			err.Field = "paths." + err.Field
+			return err
+		}
+
+		operation.Tags = api.Tags
+		operation.Description = api.Description
+		operation.Deprecated = api.Deprecated != ""
+		setOperationParams(operation, api)
+
+		if len(api.Requests) > 0 {
+			content := make(map[string]*MediaType, len(api.Requests))
+			for _, r := range api.Requests {
+				examples := make(map[string]*Example, len(r.Examples))
+				for _, exp := range r.Examples {
+					examples[exp.Mimetype] = &Example{
+						Summary: exp.Summary,
+						Value:   ExampleValue(exp.Value),
+					}
+				}
+
+				content[r.Mimetype] = &MediaType{
+					Schema:   &Schema{Schema: r.Type},
+					Examples: examples,
+				}
+			}
+
+			operation.RequestBody = &RequestBody{
+				Content: content,
+			}
+		}
+
+		operation.Responses = make(map[string]*Response, len(api.Responses))
+		for _, resp := range api.Responses {
+			r, found := operation.Responses[resp.Status]
+			if !found {
+				r = &Response{}
+				operation.Responses[resp.Status] = r
+			}
+
+			if r.Headers == nil {
+				r.Headers = make(map[string]*Header, 10)
+			}
+			for _, h := range resp.Headers {
+				r.Headers[h.Name] = &Header{
+					Description: Description(h.Summary),
+				}
+			}
+
+			if r.Content == nil {
+				r.Content = make(map[string]*MediaType, 10)
+			}
+			examples := make(map[string]*Example, len(resp.Examples))
+			for _, exp := range resp.Examples {
+				examples[exp.Mimetype] = &Example{
+					Summary: exp.Summary,
+					Value:   ExampleValue(exp.Value),
+				}
+			}
+			r.Content[resp.Mimetype] = &MediaType{
+				Schema:   &Schema{Schema: resp.Type},
+				Examples: examples,
+			}
+		}
+	} // end for doc.Apis
+
+	return nil
+}
+
+func setOperationParams(operation *Operation, api *doc.API) {
+	operation.Parameters = make([]*Parameter, 0, len(api.Params)+len(api.Queries)+len(api.Requests[0].Headers))
+
+	for _, param := range api.Params {
+		operation.Parameters = append(operation.Parameters, &Parameter{
+			Name:        param.Name,
+			IN:          ParameterINPath,
+			Description: Description(param.Summary),
+			Required:    !param.Optional,
+			Schema:      &Schema{Schema: param.Type},
+		})
+	}
+
+	for _, param := range api.Queries {
+		operation.Parameters = append(operation.Parameters, &Parameter{
+			Name:        param.Name,
+			IN:          ParameterINQuery,
+			Description: Description(param.Summary),
+			Required:    !param.Optional,
+			Schema:      &Schema{Schema: param.Type},
+		})
+	}
+
+	if len(api.Requests) > 0 {
+		for _, param := range api.Requests[0].Headers {
+			operation.Parameters = append(operation.Parameters, &Parameter{
+				Name:        param.Name,
+				IN:          ParameterINHeader,
+				Description: Description(param.Summary),
+				Required:    !param.Optional,
+			})
+		}
+	}
+}
+
+func setOperation(path *PathItem, method string) (*Operation, *Error) {
+	operation := &Operation{}
+
+	switch strings.ToUpper(method) {
+	case "GET":
+		if path.Get != nil {
+			return nil, &Error{Field: "Get", Message: "已经存在"}
+		}
+		path.Get = operation
+	case "DELETE":
+		if path.Delete != nil {
+			return nil, &Error{Field: "Delete", Message: "已经存在"}
+		}
+		path.Delete = operation
+	case "POST":
+		if path.Post != nil {
+			return nil, &Error{Field: "Post", Message: "已经存在"}
+		}
+		path.Post = operation
+	case "PUT":
+		if path.Put != nil {
+			return nil, &Error{Field: "Put", Message: "已经存在"}
+		}
+		path.Put = operation
+	case "PATCH":
+		if path.Patch != nil {
+			return nil, &Error{Field: "Patch", Message: "已经存在"}
+		}
+		path.Patch = operation
+	case "OPTIONS":
+		if path.Options != nil {
+			return nil, &Error{Field: "Options", Message: "已经存在"}
+		}
+		path.Options = operation
+	case "HEAD":
+		if path.Head != nil {
+			return nil, &Error{Field: "Head", Message: "已经存在"}
+		}
+		path.Head = operation
+	case "TRACE":
+		if path.Trace != nil {
+			return nil, &Error{Field: "Trace", Message: "已经存在"}
+		}
+		path.Trace = operation
+	}
+
+	return operation, nil
 }
 
 // JSON 输出 JSON 格式数据
