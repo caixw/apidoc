4f3366ffcc7a31f7bf28e271f94afd89dd0fa4f9
caixw <xnotepad@gmail.com>
2019-09-17 22:44:10 +0800

    删除无用的代码

--- internal/locale/locale.go
+++ internal/locale/locale.go
@@ -28,22 +28,3 @@ func init() {
 func DisplayNames() map[language.Tag]string {
 	return displayNames
 }
-
-// Locale 保存着本地化的信息，仅在需要时(调用 String)，才会被转换成当前语言。
-type Locale struct {
-	Key  message.Reference
-	Args []interface{}
-}
-
-// String fmt.Stringer
-func (l *Locale) String(p *message.Printer) string {
-	return p.Sprintf(l.Key, l.Args...)
-}
-
-// NewLocale 声明 Locale 实例
-func NewLocale(key message.Reference, v ...interface{}) *Locale {
-	return &Locale{
-		Key:  key,
-		Args: v,
-	}
-}
--- internal/locale/locale_test.go
+++ internal/locale/locale_test.go
@@ -6,8 +6,6 @@ import (
 	"testing"
 
 	"github.com/issue9/assert"
-	"golang.org/x/text/language"
-	"golang.org/x/text/message"
 )
 
 func TestDisplayNames(t *testing.T) {
@@ -18,15 +16,4 @@ func TestDisplayNames(t *testing.T) {
 		_, found := locales[tag]
 		a.True(found)
 	}
-}
-
-func TestLocale(t *testing.T) {
-	a := assert.New(t)
-
-	l := NewLocale(FlagHUsage)
-	a.NotNil(l)
-
-	l1 := l.String(message.NewPrinter(language.MustParse("zh-hans")))
-	l2 := l.String(message.NewPrinter(language.MustParse("zh-hant")))
-	a.NotEqual(l1, l2)
 }
