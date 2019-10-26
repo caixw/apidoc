8e84ecf0e27fbba11ebb47fe3b96cb6f25c2eb94
caixw <xnotepad@gmail.com>
2019-10-26 14:10:46 +0800

    修正 build.cmd 错误
    因为 windows 默认编码为 cp936，build.cmd 的编码为 utf-8，为了通用，该文件不能包含了非 ascii 字符。

--- build/build.cmd
+++ build/build.cmd
@@ -1,19 +1,14 @@
 :: SPDX-License-Identifier: MIT
 
-:: 代码的主目录，变量赋值时，等号两边不能有空格。
 set wd=%~dp0\..\cmd\apidoc
 
-:: 程序所在的目录
 set mainPath=github.com\caixw\apidoc
 
-:: 需要修改变量的名名，若为 main，则指接使用 main，而不是全地址
 set varsPath=%mainPath%\v5\internal\vars
 
-:: 当前日期，格式为 YYYYMMDD
 set builddate=%date:~0,4%%date:~5,2%%date:~8,2% 
 
-:: 获取最后一条前交的 hash
 for /f "delims=" %%t in ('git rev-parse HEAD') do set hash=%%t
 
-echo 开始编译
-%GOROOT%\bin\go build -o %wd%\apidoc.exe -ldflags "-X %varsPath%.buildDate=%builddate% -X %varsPath%.commitHash=%hash%" -v %mainPath%
+echo compile
+go build -o %wd%\apidoc.exe -ldflags "-X %varsPath%.buildDate=%builddate% -X %varsPath%.commitHash=%hash%" -v %wd%
