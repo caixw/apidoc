c497832b810acc41f66d3f4d25753d16dde276a2
caixw <xnotepad@gmail.com>
2019-11-05 22:05:50 +0800

    添加测试内容

--- apidoc_test.go
+++ apidoc_test.go
@@ -3,17 +3,49 @@
 package apidoc
 
 import (
+	"bytes"
+	"strconv"
 	"testing"
 
 	"github.com/issue9/assert"
 	"github.com/issue9/version"
 
 	"github.com/caixw/apidoc/v5/internal/vars"
+	"github.com/caixw/apidoc/v5/message"
 )
+
+func buildMessageHandle() (*bytes.Buffer, message.HandlerFunc) {
+	buf := new(bytes.Buffer)
+
+	return buf, func(msg *message.Message) {
+		buf.WriteString(strconv.Itoa(int(msg.Type)))
+		buf.WriteString(msg.Message)
+	}
+}
 
 func TestVersion(t *testing.T) {
 	a := assert.New(t)
 
 	a.Equal(Version(), vars.Version())
 	a.True(version.SemVerValid(Version()))
+}
+
+func TestMake(t *testing.T) {
+	a := assert.New(t)
+
+	out, f := buildMessageHandle()
+	h := message.NewHandler(f)
+	Make(h, "./docs/example", true)
+	a.Empty(out.Bytes())
+}
+
+func TestMakeBuffer(t *testing.T) {
+	a := assert.New(t)
+
+	out, f := buildMessageHandle()
+	h := message.NewHandler(f)
+	buf, dur := MakeBuffer(h, "./docs/example")
+	a.Empty(out.Bytes()).
+		True(dur > 0).
+		True(buf.Len() > 0)
 }
