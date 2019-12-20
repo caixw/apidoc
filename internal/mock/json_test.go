5da91f81f52e808a794dafe29c5ae31026a1d076
caixw <xnotepad@gmail.com>
2019-12-20 22:35:10 +0800

    添加 jsonValidator 用于对提交的 JSON 数据作格式验证

--- internal/mock/json_test.go
+++ internal/mock/json_test.go
@@ -3,6 +3,8 @@
 package mock
 
 import (
+	"encoding/json"
+	"strings"
 	"testing"
 
 	"github.com/issue9/assert"
@@ -10,122 +12,118 @@ import (
 	"github.com/caixw/apidoc/v5/doc"
 )
 
-func init() {
-	test = true
+type jsonTester struct {
+	Title string
+	Type  *doc.Request
+	Data  string
 }
 
-type builder struct {
-	input  *doc.Request
-	output string
-	err    bool
-}
-
-func testBuilder(a *assert.Assertion, builders []*builder) {
-	for _, builder := range builders {
-		data, err := buildJSON(builder.input)
-		if builder.err {
-			a.Error(err).Nil(data)
-		} else {
-			a.NotError(err).Equal(string(data), builder.output)
-		}
-	}
-}
-
-func TestBuildJSON(t *testing.T) {
-	a := assert.New(t)
-
-	data := []*builder{
-		{
-			input:  nil,
-			output: "null",
+var jsonTestData = []*jsonTester{
+	{
+		Title: "nil",
+		Type:  nil,
+		Data:  "null",
+	},
+	{
+		Title: "doc.None",
+		Type:  &doc.Request{Type: doc.None},
+		Data:  "null",
+	},
+	{
+		Title: "doc.Request{}",
+		Type:  &doc.Request{},
+		Data:  "",
+	},
+	{
+		Title: "number",
+		Type:  &doc.Request{Type: doc.Number},
+		Data:  "1024",
+	},
+	{ // array
+		Title: "[bool]",
+		Type: &doc.Request{
+			Type:  doc.Bool,
+			Array: true,
 		},
-		{
-			input:  &doc.Request{},
-			output: "",
-		},
-		{ // array
-			input: &doc.Request{
-				Type:  doc.Bool,
-				Array: true,
-			},
-			output: `[
+		Data: `[
     true,
     true,
     true,
     true,
     true
 ]`,
-		},
-		{
-			input: &doc.Request{
-				Type: doc.Bool,
-			},
-			output: "true",
-		},
-		{ // Object
-			input: &doc.Request{
-				Type: doc.Object,
-				Items: []*doc.Param{
-					{
-						Type: doc.String,
-						Name: "name",
-					},
-					{
-						Type: doc.Number,
-						Name: "id",
-					},
+	},
+	{
+		Title: "bool",
+		Type:  &doc.Request{Type: doc.Bool},
+		Data:  "true",
+	},
+	{ // Object
+		Title: "Object",
+		Type: &doc.Request{
+			Type: doc.Object,
+			Items: []*doc.Param{
+				{
+					Type: doc.String,
+					Name: "name",
+				},
+				{
+					Type: doc.Number,
+					Name: "id",
 				},
 			},
-			output: `{
+		},
+		Data: `{
     "name": "1024",
     "id": 1024
 }`,
-		},
+	},
 
-		{ // 各类型混合
-			input: &doc.Request{
-				Type: doc.Object,
-				Items: []*doc.Param{
-					{
-						Type: doc.String,
-						Name: "name",
-					},
-					{
-						Type: doc.Number,
-						Name: "id",
-					},
-					{
-						Type: doc.Object,
-						Name: "group",
-						Items: []*doc.Param{
-							{
-								Type: doc.String,
-								Name: "name",
-							},
-							{
-								Type: doc.Number,
-								Name: "id",
-							},
-							{
-								Name:  "tags",
-								Array: true,
-								Type:  doc.Object,
-								Items: []*doc.Param{
-									{
-										Type: doc.String,
-										Name: "name",
-									},
-									{
-										Type: doc.Number,
-										Name: "id",
-									},
-								},
-							}, // end tags
-						},
-					}, // end group
+	{ // 各类型混合
+		Title: "Object with array",
+		Type: &doc.Request{
+			Type: doc.Object,
+			Items: []*doc.Param{
+				{
+					Type: doc.String,
+					Name: "name",
 				},
+				{
+					Type: doc.Number,
+					Name: "id",
+				},
+				{
+					Type: doc.Object,
+					Name: "group",
+					Items: []*doc.Param{
+						{
+							Type: doc.String,
+							Name: "name",
+						},
+						{
+							Type: doc.Number,
+							Name: "id",
+						},
+						{
+							Name:  "tags",
+							Array: true,
+							Type:  doc.Object,
+							Items: []*doc.Param{
+								{
+									Type: doc.String,
+									Name: "name",
+								},
+								{
+									Type: doc.Number,
+									Name: "id",
+								},
+							},
+						}, // end tags
+					},
+				}, // end group
 			},
-			output: `{
+		},
+		Data: `{
     "name": "1024",
     "id": 1024,
     "group": {
@@ -155,8 +153,63 @@ func TestBuildJSON(t *testing.T) {
         ]
     }
 }`,
-		},
+	},
+}
+
+func TestJSONValidator_find(t *testing.T) {
+	item := jsonTestData[len(jsonTestData)-1]
+
+	a := assert.New(t)
+	v := &jsonValidator{
+		param:   item.Type.ToParam(),
+		decoder: json.NewDecoder(strings.NewReader(item.Data)),
 	}
 
-	testBuilder(a, data)
+	v.names = []string{}
+	p := v.find()
+	a.Equal(p, v.param)
+
+	v.names = nil
+	p = v.find()
+	a.Equal(p, v.param)
+
+	v.names = []string{""}
+	p = v.find()
+	a.Nil(p)
+
+	v.names = []string{"name"}
+	p = v.find()
+	a.NotNil(p).Equal(p.Type, doc.String)
+
+	v.names = []string{"not-exists"}
+	p = v.find()
+	a.Nil(p)
+
+	v.names = []string{"group", "id"}
+	p = v.find()
+	a.NotNil(p).Equal(p.Type, doc.Number)
+
+	v.names = []string{"group", "tags", "id"}
+	p = v.find()
+	a.NotNil(p).Equal(p.Type, doc.Number)
+}
+
+func TestValidJSON(t *testing.T) {
+	a := assert.New(t)
+
+	for _, item := range jsonTestData {
+		err := validJSON(item.Type, []byte(item.Data))
+		a.NotError(err, "测试 %s 时返回错误值 %s", item.Title, err)
+	}
+}
+
+func TestBuildJSON(t *testing.T) {
+	a := assert.New(t)
+
+	for _, item := range jsonTestData {
+		data, err := buildJSON(item.Type)
+
+		a.NotError(err, "测试 %s 返回了错误值 %s", item.Title, err).
+			Equal(string(data), item.Data)
+	}
 }
