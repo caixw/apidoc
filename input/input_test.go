c98633ac9736287d3fcf4fddc075bf8c5f899e77
caixw <xnotepad@gmail.com>
2020-03-01 10:48:12 +0800

    test(input): 添加对 Parse 和 ParseFile 的测试内容

--- input/input_test.go
+++ input/input_test.go
@@ -8,7 +8,52 @@ import (
 	"github.com/issue9/assert"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/simplifiedchinese"
+
+	"github.com/caixw/apidoc/v6/message/messagetest"
 )
+
+func TestParse(t *testing.T) {
+	a := assert.New(t)
+
+	blocks := make(chan Block, 100)
+	erro, _, h := messagetest.MessageHandler()
+	php := &Options{
+		Lang:      "php",
+		Dir:       "./testdata",
+		Recursive: true,
+		Encoding:  "gbk",
+	}
+
+	c := &Options{
+		Lang:      "c++",
+		Dir:       "./testdata",
+		Recursive: true,
+	}
+	a.NotError(Parse(blocks, h, php, c))
+	close(blocks)
+
+	a.Equal(5, len(blocks))
+	a.Empty(erro.String())
+}
+
+func TestParseFile(t *testing.T) {
+	a := assert.New(t)
+
+	blocks := make(chan Block, 100)
+	erro, _, h := messagetest.MessageHandler()
+	o := &Options{
+		Lang:      "php",
+		Dir:       "./testdata",
+		Recursive: true,
+		Encoding:  "gbk",
+	}
+	a.NotError(o.sanitize())
+	ParseFile(blocks, h, "./testdata/testfile.c", o)
+	close(blocks)
+
+	a.Equal(2, len(blocks))
+	a.Empty(erro.String())
+}
 
 func TestReadFile(t *testing.T) {
 	a := assert.New(t)
--- input/testdata/gbk.php
+++ input/testdata/gbk.php
@@ -1,3 +1,10 @@
 <?php
 
 echo '这是一个 GBK 编码的文件';
+
+
+// <api method="GET">
+// <path path="/apis" />
+// <description>中文描述</description>
+// <server>test</server>
+// </api>
