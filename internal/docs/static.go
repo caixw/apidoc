5bd945129eb8143dd2a8262de69ee25cf058b825
caixw <xnotepad@gmail.com>
2020-03-07 03:17:33 +0800

    refactor: go generate

--- internal/docs/static.go
+++ internal/docs/static.go
@@ -42,7 +42,7 @@ var data = []*FileInfo{{
 		Content: []byte(`<?xml version="1.0" encoding="UTF-8"?>
 
 <?xml-stylesheet type="text/xsl" href="../v6/apidoc.xsl"?>
-<apidoc apidoc="6.0.0" created="2020-03-05T00:27:52+08:00" version="1.1.1">
+<apidoc apidoc="6.0.0" created="2020-03-07T03:16:53+08:00" version="1.1.1">
 	<title>示例文档</title>
 	<description type="html"><![CDATA[
  
