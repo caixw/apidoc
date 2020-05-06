850985a99faa455437f4e7478bd90628bc389ae6
caixw <xnotepad@gmail.com>
2020-05-06 23:36:44 +0800

    feat(internal/ast): 将 spec 的验证移至 ast

--- internal/ast/sanitize.go
+++ internal/ast/sanitize.go
@@ -8,9 +8,133 @@ import (
 
 	"github.com/issue9/is"
 
+	"github.com/caixw/apidoc/v7/core"
 	"github.com/caixw/apidoc/v7/internal/locale"
 	"github.com/caixw/apidoc/v7/internal/token"
 )
+
+// Sanitize token.Sanitizer
+func (api *API) Sanitize(p *token.Parser) error {
+	for _, header := range api.Headers { // 报头不能为 object
+		if header.Type.V() == TypeObject {
+			return p.NewError(header.Type.Start, header.Type.End, "header", locale.ErrInvalidValue)
+		}
+	}
+
+	return nil
+}
+
+// Sanitize token.Sanitizer
+func (e *Enum) Sanitize(p *token.Parser) error {
+	if e.Description.V() == "" && e.Summary.V() == "" {
+		return p.NewError(e.Start, e.End, "summary", locale.ErrRequired)
+	}
+
+	return nil
+}
+
+// Sanitize token.Sanitizer
+func (p *Path) Sanitize(pp *token.Parser) error {
+	params, err := parsePath(p.Path.V())
+	if err != nil {
+		return pp.NewError(p.Path.Start, p.Path.End, "path", locale.ErrInvalidFormat)
+	}
+	if len(params) != len(p.Params) {
+		return pp.NewError(p.Start, p.End, "path", locale.ErrPathNotMatchParams)
+	}
+	for _, param := range p.Params {
+		if _, found := params[param.Name.V()]; !found {
+			return pp.NewError(param.Start, param.End, "path", locale.ErrPathNotMatchParams)
+		}
+	}
+
+	// 路径参数和查询参数不能为 object
+	for _, item := range p.Params {
+		if item.Type.V() == TypeObject {
+			return pp.NewError(item.Start, item.End, "type", locale.ErrInvalidValue)
+		}
+	}
+	for _, q := range p.Queries {
+		if q.Type.V() == TypeObject {
+			return pp.NewError(q.Start, q.End, "type", locale.ErrInvalidValue)
+		}
+	}
+
+	return nil
+}
+
+func parsePath(path string) (params map[string]struct{}, err error) {
+	start := -1
+	for i, b := range path {
+		switch b {
+		case '{':
+			if start != -1 {
+				return nil, locale.NewError(locale.ErrInvalidFormat)
+			}
+
+			start = i + 1
+		case '}':
+			if start == -1 {
+				return nil, locale.NewError(locale.ErrInvalidFormat)
+			}
+
+			if params == nil {
+				params = make(map[string]struct{}, 3)
+			}
+			params[path[start:i]] = struct{}{}
+			start = -1
+		default:
+		}
+	}
+
+	if start != -1 { // 没有结束符号
+		return nil, locale.NewError(locale.ErrInvalidFormat)
+	}
+
+	return params, nil
+}
+
+// Sanitize token.Sanitizer
+func (r *Request) Sanitize(p *token.Parser) error {
+	if r.Type.V() == TypeObject && len(r.Items) == 0 {
+		return p.NewError(r.Start, r.End, "param", locale.ErrRequired)
+	}
+
+	// 判断 enums 的值是否相同
+	if rng, found := getDuplicateEnum(r.Enums); found {
+		return p.NewError(rng.Start, rng.End, "enum", locale.ErrDuplicateValue)
+	}
+
+	if err := chkEnumsType(r.Type, r.Enums, p); err != nil {
+		return err
+	}
+
+	if err := checkXML(r.Array.V(), len(r.Items) > 0, &r.XML, p); err != nil {
+		return err
+	}
+
+	if r.Mimetype.V() != "" {
+		for _, exp := range r.Examples {
+			if exp.Mimetype.V() != r.Mimetype.V() {
+				return p.NewError(r.Mimetype.Start, r.Mimetype.End, "mimetype", locale.ErrInvalidValue)
+			}
+		}
+	}
+
+	// 报头不能为 object
+	for _, header := range r.Headers {
+		if header.Type.V() == TypeObject {
+			return p.NewError(header.Type.Start, header.Type.End, "type", locale.ErrInvalidValue)
+		}
+	}
+
+	// 判断 items 的值是否相同
+	if rng, found := getDuplicateItems(r.Items); found {
+		return p.NewError(rng.Start, rng.End, "param", locale.ErrDuplicateValue)
+	}
+
+	return nil
+}
 
 // Sanitize token.Sanitizer
 func (p *Param) Sanitize(pp *token.Parser) error {
@@ -22,8 +146,8 @@ func (p *Param) Sanitize(pp *token.Parser) error {
 	}
 
 	// 判断 enums 的值是否相同
-	if key := getDuplicateEnum(p.Enums); key != "" {
-		return pp.NewError(p.Start, p.End, "enum", locale.ErrDuplicateValue)
+	if r, found := getDuplicateEnum(p.Enums); found {
+		return pp.NewError(r.Start, r.End, "enum", locale.ErrDuplicateValue)
 	}
 
 	if err := chkEnumsType(p.Type, p.Enums, pp); err != nil {
@@ -31,8 +155,8 @@ func (p *Param) Sanitize(pp *token.Parser) error {
 	}
 
 	// 判断 items 的值是否相同
-	if key := getDuplicateItems(p.Items); key != "" {
-		return pp.NewError(p.Start, p.End, "param", locale.ErrDuplicateValue)
+	if r, found := getDuplicateItems(p.Items); found {
+		return pp.NewError(r.Start, r.End, "param", locale.ErrDuplicateValue)
 	}
 
 	if err := checkXML(p.Array.V(), len(p.Items) > 0, &p.XML, pp); err != nil {
@@ -73,44 +197,44 @@ func chkEnumsType(t *TypeAttribute, enums []*Enum, p *token.Parser) error {
 }
 
 // 返回重复枚举的值
-func getDuplicateEnum(enums []*Enum) string {
+func getDuplicateEnum(enums []*Enum) (core.Range, bool) {
 	if len(enums) == 0 {
-		return ""
+		return core.Range{}, false
 	}
 
-	es := make([]string, 0, len(enums))
+	es := make([]*Enum, 0, len(enums))
 	for _, e := range enums {
-		es = append(es, e.Value.V())
+		es = append(es, e)
 	}
-	sort.SliceStable(es, func(i, j int) bool { return es[i] > es[j] })
+	sort.SliceStable(es, func(i, j int) bool { return es[i].Value.V() > es[j].Value.V() })
 
 	for i := 1; i < len(es); i++ {
-		if es[i] == es[i-1] {
-			return es[i]
+		if es[i].Value.V() == es[i-1].Value.V() {
+			return es[i].Range, true
 		}
 	}
 
-	return ""
+	return core.Range{}, false
 }
 
-func getDuplicateItems(items []*Param) string {
+func getDuplicateItems(items []*Param) (core.Range, bool) {
 	if len(items) == 0 {
-		return ""
+		return core.Range{}, false
 	}
 
-	es := make([]string, 0, len(items))
+	es := make([]*Param, 0, len(items))
 	for _, e := range items {
-		es = append(es, e.Name.V())
+		es = append(es, e)
 	}
-	sort.SliceStable(es, func(i, j int) bool { return es[i] > es[j] })
+	sort.SliceStable(es, func(i, j int) bool { return es[i].Name.V() > es[j].Name.V() })
 
 	for i := 1; i < len(es); i++ {
-		if es[i] == es[i-1] {
-			return es[i]
+		if es[i].Name.V() == es[i-1].Name.V() {
+			return es[i].Range, true
 		}
 	}
 
-	return ""
+	return core.Range{}, false
 }
 
 func checkXML(isArray, hasItems bool, xml *XML, p *token.Parser) error {
@@ -160,8 +284,6 @@ func checkXML(isArray, hasItems bool, xml *XML, p *token.Parser) error {
 
 // Sanitize 检测内容是否合法
 func (doc *APIDoc) Sanitize(p *token.Parser) error {
-	// TODO
-
 	// doc.Apis 是多线程导入的，无法保证其顺序，
 	// 此处可以保证输出内容是按一定顺序排列的。
 	sort.SliceStable(doc.Apis, func(i, j int) bool {
@@ -177,6 +299,7 @@ func (doc *APIDoc) Sanitize(p *token.Parser) error {
 	for _, api := range doc.Apis {
 		if api.doc == nil {
 			api.doc = doc // 保证单文件的文档能正常解析
+			api.URI = doc.URI
 		}
 		if err := api.sanitizeTags(p); err != nil {
 			return err
--- internal/ast/sanitize_test.go
+++ internal/ast/sanitize_test.go
@@ -4,7 +4,10 @@ package ast
 
 import "github.com/caixw/apidoc/v7/internal/token"
 
-var(
+var (
 	_ token.Sanitizer = &Param{}
+	_ token.Sanitizer = &Request{}
 	_ token.Sanitizer = &APIDoc{}
+	_ token.Sanitizer = &Path{}
+	_ token.Sanitizer = &Enum{}
 )
