5e2131c1dd5af23e6a921bbfbf38e96caca94ba8
caixw <xnotepad@gmail.com>
2020-05-13 00:49:31 +0800

    docs(internal/token): 修正文档错误

--- internal/token/token.go
+++ internal/token/token.go
@@ -6,23 +6,15 @@
 //
 // 标签属性分为 4 个字段，其中前三个是必填的：
 //  apidoc:"name,node-type,usage-key,omitempty"
-// 第一个标签指定名称，如果为空，则直接采用字段的名称；
-// 第二个标签指定标签的类型，可以是 elem 表示子元素，attr 表示属性，
-// cdata 表示当前 XML 元素子元素内容 CDATA 数据，content
-// 表示当前 XML 的子元素内容作为字符串保存至 content；
-// 第三个元素用于指定当前元素的使用说明的本地化 ID，
-// 加载后调用相关的方法会被翻译成本地化的语言内容返回；
-// 第四个参数表示是否忽略空值，与标准库的 omitempty 相同功能，默认为 false。
-//
-// 根对象
-//
-// 根对象必须添加一个 RootName 字段指定根名称以其它属性：
-//  type Root struct {
-//      RootName struct{} `apidoc:"root,elem,usage-key"`
-//      // 其它字段 ...
-//  }
-// 其 apidoc 标签值与其它的标签值格式相同，但是只有第一和第三个值是真实有效果的，
-// 另两个值会被忽略。
+// name 表示当前标签的名称；
+// node-type 表示当前节点的类型，可以是以下值：
+//  - elem 表示这是一个子元素；
+//  - attr 表示为一个 XML 属性；
+//  - cdata 表示为 CDATA 数据；
+//  - content 表示为普通的字符串值；
+//  - meta 表示这个字段仅用于描述当前元素的元数据，比如元素的名称等；
+// usage-key 指定了当前元素的翻译项；
+// omitempty 表示当前值为空时，是否可以忽略。
 package token
 
 import (
@@ -79,7 +71,7 @@ type Comment struct {
 	Value String
 }
 
-// 这些常量对应 BaseTag 中相关字段的名称
+// 这些常量对应 Base* 中相关字段的名称
 const (
 	rangeName         = "Range"
 	usageKeyName      = "UsageKey"
