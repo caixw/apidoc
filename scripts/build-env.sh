e2fa3c2de047bd95b675db8d79b9bf22c655b9e1
caixw <xnotepad@gmail.com>
2020-05-18 15:37:50 +0800

    build: 调整 build 脚本
    根据版本相关功能调整指向新地址

--- scripts/build-env.sh
+++ scripts/build-env.sh
@@ -11,6 +11,6 @@ date=`date -u '+%Y%m%d'`
 hash=`git rev-parse HEAD`
 
 # 需要修改变量的地址，若为 main，则指接使用 main，而不是全地址
-path=github.com/caixw/apidoc/v7/internal/vars
+path=github.com/caixw/apidoc/v7/core
 
 ldflags="-X ${path}.buildDate=${date} -X ${path}.commitHash=${hash}"
