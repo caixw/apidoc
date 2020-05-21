80df9c8154c12c085f9e01c7193749d79ff7d327
caixw <xnotepad@gmail.com>
2020-05-21 17:29:57 +0800

    fix(internal/openapi): 修正字段名错误

--- internal/openapi/openapi.go
+++ internal/openapi/openapi.go
@@ -39,7 +39,7 @@ type Components struct {
 	Examples        map[string]*Example        `json:"examples,omitempty" yaml:"examples,omitempty"`
 	RequestBodies   map[string]*RequestBody    `json:"requestBodies,omitempty" yaml:"requestBodies,omitempty"`
 	Headers         map[string]*Header         `json:"headers,omitempty" yaml:"headers,omitempty"`
-	SecuritySchemes map[string]*SecurityScheme `json:"securitySechemes,omitempty" yaml:"securitySechemes,omitempty"`
+	SecuritySchemes map[string]*SecurityScheme `json:"securitySchemes,omitempty" yaml:"securitySchemes,omitempty"`
 	Links           map[string]*Link           `json:"links,omitempty" yaml:"links,omitempty"`
 	Callbacks       map[string]*Callback       `json:"callbacks,omitempty" yaml:"callbacks,omitempty"`
 }
