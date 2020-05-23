37e152705b61835b484ce0227c3466082b3887ce
caixw <xnotepad@gmail.com>
2020-05-24 01:18:42 +0800

    refactor(internal/lsp): 合并文件

--- internal/lsp/textdocument.go
+++ internal/lsp/textdocument.go
@@ -3,8 +3,13 @@
 package lsp
 
 import (
+	"fmt"
+	"reflect"
+
+	"github.com/caixw/apidoc/v7/core"
 	"github.com/caixw/apidoc/v7/internal/locale"
 	"github.com/caixw/apidoc/v7/internal/lsp/protocol"
+	"github.com/caixw/apidoc/v7/internal/token"
 )
 
 // textDocument/didOpen
@@ -46,6 +51,33 @@ func (s *server) textDocumentDidChange(notify bool, in *protocol.DidChangeTextDo
 //
 // https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textDocument_hover
 func (s *server) textDocumentHover(notify bool, in *protocol.HoverParams, out *protocol.Hover) error {
-	*out = *(s.search(in))
+	fmt.Println("INFO")
+	for _, f := range s.folders {
+		f.searchHover(in.TextDocument.URI, in.TextDocumentPositionParams.Position, out)
+
+	}
 	return nil
+}
+
+func (f *folder) searchHover(uri core.URI, pos core.Position, hover *protocol.Hover) {
+	var tip *token.Tip
+	if f.doc.URI == uri {
+		tip = token.SearchUsage(reflect.ValueOf(f.doc), pos, "APIs")
+	}
+
+	for _, api := range f.doc.APIs {
+		if api.URI == uri {
+			if tip = token.SearchUsage(reflect.ValueOf(api), pos); tip != nil {
+				break
+			}
+		}
+	}
+
+	if tip != nil {
+		hover.Range = tip.Range
+		hover.Contents = protocol.MarkupContent{
+			Kind:  protocol.MarkupKinMarkdown,
+			Value: tip.Usage,
+		}
+	}
 }
