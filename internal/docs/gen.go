35f40621caceee53610dd5015cbcbdda16ac4e88
caixw <xnotepad@gmail.com>
2020-06-03 02:17:01 +0800

    fix(internal/docs): 修正 generate 脚本错误

--- internal/docs/gen.go
+++ internal/docs/gen.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 //go:generate go run ./make_site.go
-//go:generate go run ../../cmd/apidoc/main.go build ../../docs/example
+//go:generate go run ../../cmd/apidoc/ build -d=../../docs/example
 
 // make_static 需要将以上的内容打包到 static.go，所以要放在最后调用。
 //go:generate go run ./make_static.go
